0824743a6f68c2e90b19748bb3b634c8932e4fa0
610494 <i0905108390@gmail.com>
2023-04-24 22:29:20 +0800

    feat: playing room drag tile

---
+++ Panda-Mahjong/Assets/Script/DragTile.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragTile : MonoBehaviour, IBeginDragHandler , IDragHandler, IEndDragHandler
+{
+    public Transform tilePool, hand;
+    private Transform self;
+    private Vector3 oriPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) 
+    {
+        if(self.transform.parent == hand)
+        {
+            oriPosition = self.transform.position;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData) 
+    {
+        if(self.transform.parent == hand)
+        {
+            self.transform.position = eventData.position;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData) 
+    {
+        if(transform.localPosition.y > 0)
+        {
+            self.transform.parent = tilePool;
+        }
+        else
+        {
+            self.transform.position = oriPosition;
+        }
+    }
+}
