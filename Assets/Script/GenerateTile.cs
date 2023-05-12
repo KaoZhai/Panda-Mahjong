36802c8c06059c00cd4751c42e3f8172ddb2cb62
aokblast <i19780219111@kimo.com>
2023-05-12 14:58:07 +0800

    feat: add all project file

---
+++ Assets/Script/GenerateTile.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GenerateTile : MonoBehaviour
+{
+    public GameObject mahjongPrefab; // 麻將Prefab
+    public Vector3 startPosition; // 生成起始位置
+    public float spacing = 2.0f; // 麻將間距
+    public Transform parent;
+
+    void Start()
+    {
+        GenerateMahjong();
+    }
+
+    void GenerateMahjong()
+    {
+        int cardIndex = 1;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                GameObject mahjong = Instantiate(mahjongPrefab, startPosition + new Vector3(j * spacing, 0, i * spacing), Quaternion.identity, parent);
+                GameObject face = mahjong.transform.Find("Face").gameObject;
+                Image faceImage = face.GetComponent<Image>();
+                
+                Sprite img = Resources.Load<Sprite>("Image/Mahjong/" + cardIndex.ToString());
+                if (img) 
+                {
+                    faceImage.sprite = img;
+                    Debug.Log("成功設定圖像" + "Image/Mahjong/" + cardIndex.ToString());
+                }
+                else
+                {
+                    Debug.Log("無法設定圖像" + "Image/Mahjong/" + cardIndex.ToString());
+                }
+                mahjong.name = string.Format("Mahjong_{0}_{1}", i + 1, j);
+                ++cardIndex;
+            }
+        }
+    }
+}
