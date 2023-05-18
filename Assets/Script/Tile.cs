5a568fa481cfdca4664944e4cf51caf0657ec21b
ra890927 <happy.890927410@gmail.com>
2023-05-18 16:33:52 +0800

    use getter setter

--- Assets/Script/Tile.cs
+++ Assets/Script/Tile.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace Game.Tile 
+namespace Game.Tile
 {
     public enum TileType
     {
-        Flower, 
+        Flower,
         Season,
         Wind,
-        Dragon, 
+        Dragon,
         Character,
         Bamboo,
         Dot
     }
 
-    public class Tile : MonoBehaviour, IBeginDragHandler , IDragHandler, IEndDragHandler
+    public class Tile : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         // {tile_type}_{tile_number}_{1..4}
         private string id;
@@ -28,33 +28,33 @@ namespace Game.Tile
         private TableManager tableManager;
         private Transform self;
         private Vector3 oriPosition;
-        
+
         void Start()
         {
             self = GetComponent<Transform>();
         }
 
-        public void OnBeginDrag(PointerEventData eventData) 
+        public void OnBeginDrag(PointerEventData eventData)
         {
-            if(IsValidDrag(self.transform.parent, tableManager.GetLocalPlayerId()))
+            if (IsValidDrag(self.transform.parent, tableManager.GetLocalPlayerId()))
             {
                 oriPosition = self.transform.position;
             }
         }
 
-        public void OnDrag(PointerEventData eventData) 
+        public void OnDrag(PointerEventData eventData)
         {
-            if(IsValidDrag(self.transform.parent, tableManager.GetLocalPlayerId()))
+            if (IsValidDrag(self.transform.parent, tableManager.GetLocalPlayerId()))
             {
                 self.transform.position = eventData.position;
             }
         }
 
-        public void OnEndDrag(PointerEventData eventData) 
+        public void OnEndDrag(PointerEventData eventData)
         {
-            if(IsValidDrag(self.transform.parent, tableManager.GetLocalPlayerId()))
+            if (IsValidDrag(self.transform.parent, tableManager.GetLocalPlayerId()))
             {
-                if(transform.localPosition.y > 0)
+                if (transform.localPosition.y > 0)
                 {
                     self.transform.SetParent(tilePool);
                     tableManager.NextPlayer();
@@ -71,14 +71,60 @@ namespace Game.Tile
             return parent == hand && id == playerId;
         }
 
-        public int GetPlayerId()
+        public string PlayerId
         {
-            return playerId;
+            get
+            {
+                return playerId;
+            }
+            set
+            {
+                playerId = value;
+            }
         }
 
-        public void SetPlayerId(int id)
+        public TileType TileType
         {
-            playerId = id;
+            get
+            {
+                return tileType;
+            }
+            set
+            {
+                tileType = value;
+            }
+        }
+
+        public int TileNumber
+        {
+            get
+            {
+                return tileNumber;
+            }
+            set
+            {
+                tileNumber = value;
+            }
+        }
+
+        public int CardFaceIndex
+        {
+            get
+            {
+                return cardFaceIndex;
+            }
+            set
+            {
+                cardFaceIndex = value;
+            }
+        }
+
+        public string TileId
+        {
+            get
+            {
+                return id;
+            }
         }
 
         public void SetHand(Transform hand)
@@ -94,46 +140,6 @@ namespace Game.Tile
         public void SetTableManager(TableManager tableManager)
         {
             this.tableManager = tableManager;
-        }
-
-        public Game.Tile.TileType GetTileType()
-        {
-            return tileType;
-        }
-
-        public void SetTileType(TileType type)
-        {
-            tileType = type;
-        }
-
-        public int GetTileNumber()
-        {
-            return tileNumber;
-        }
-
-        public void SetTileNumber(int number)
-        {
-            tileNumber = number;
-        }
-
-        public int GetCardFaceIndex()
-        {
-            return cardFaceIndex;
-        }
-
-        public void SetCardFaceIndex(int cardFaceIndex)
-        {
-            this.cardFaceIndex = cardFaceIndex;
-        }
-
-        public string GetTileId()
-        {
-            return id;
-        }
-
-        public void SetTileId(string id)
-        {
-            this.id = id;
         }
     }
 }
