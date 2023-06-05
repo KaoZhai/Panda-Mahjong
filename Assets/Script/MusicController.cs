130edf8966ea75095a1ca18808c7965111b962b2
610494 <i0905108390@gmail.com>
2023-06-05 22:54:45 +0800

    feat:MusicController

---
+++ Assets/Script/MusicController.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Music
+{
+    public enum EnumMusic
+    {
+        Start,
+        Lobby,
+        PlayingRoom
+    }
+
+    public class MusicController : MonoBehaviour
+    {
+        private Dictionary<EnumMusic, AudioClip> musicDict = new Dictionary<EnumMusic, AudioClip>();
+
+        [SerializeField] private AudioClip startMusic = null;
+        [SerializeField] private AudioClip lobbyMusic = null;
+        [SerializeField] private AudioClip playingRoomMusic = null;
+
+        void Start() 
+        {
+            AddMusic(EnumMusic.Start,startMusic);
+            AddMusic(EnumMusic.Lobby,lobbyMusic);
+            AddMusic(EnumMusic.PlayingRoom,playingRoomMusic);
+        }
+
+        public void AddMusic(EnumMusic enumMusic, AudioClip music)
+        {
+            Debug.Log($"add music: {enumMusic}");
+            if (!musicDict.ContainsKey(enumMusic))
+                musicDict.Add(enumMusic, music);
+        }
+
+        public void PlayMusic(EnumMusic enumMusic)
+        {
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.Stop();
+            switch (enumMusic)
+            {
+                
+                case EnumMusic.Start:
+                    try
+                    {
+                        audioSource.clip = musicDict[EnumMusic.Start];
+                        audioSource.Play();
+                    }
+                    catch (ArgumentException error)
+                    {
+                        Debug.Log(error);
+                    }
+                    break;
+                case EnumMusic.Lobby:
+                    try
+                    {
+                        audioSource.clip = musicDict[EnumMusic.Lobby];
+                        audioSource.Play();
+                    }
+                    catch (ArgumentException error)
+                    {
+                        Debug.Log(error);
+                    }
+                    break;
+                case EnumMusic.PlayingRoom:
+                    try
+                    {
+                        audioSource.clip = musicDict[EnumMusic.PlayingRoom];
+                        audioSource.Play();
+                    }
+                    catch (ArgumentException error)
+                    {
+                        Debug.Log(error);
+                    }
+                    break;
+                
+            }
+        }
+    }
+
+}
