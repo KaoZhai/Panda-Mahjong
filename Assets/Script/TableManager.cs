7b6b338ed8d90258e63306148c19e60b4cd756da
ShihHeng <happy.890927410@gmail.com>
2023-06-07 05:08:53 +0800

    error: wait for fix id instead of index

--- Assets/Script/TableManager.cs
+++ Assets/Script/TableManager.cs
@@ -1,65 +1,172 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Game.Core;
+using Utils;
 
-namespace Game.Play {
-    
+namespace Game.Play
+{
+
     public class TableManager : MonoBehaviour
     {
         public static TableManager Instance => null;
 
-        [SerializeField] private List<Player> players = new List<Player>();
+        private Dictionary<string, Player> players = new Dictionary<string, Player>();
+        private Dictionary<string, PlayerController> playerControllerDict = new Dictionary<string, PlayerController>();
         [SerializeField] private TileWall tileWall;
 
-        private int activePlayerIndex;
+        private string activePlayerId;
 
         [SerializeField] private GameObject winningBtn, chiBtn, pongBtn, kongBtn;
         [SerializeField] private GameObject roundPoints;
         [SerializeField] private Text points;
         [SerializeField] private List<Text> pointsChangeText;
         [SerializeField] private List<Text> winningType;
-        
+        [SerializeField] private Transform contentTrans;
+        [SerializeField] private Player playerPrefab;
+        [SerializeField] private RoomManager roomManager;
+
 
         public TileWall TileWall => tileWall;
 
         public GameObject LastTile { set; get; }
 
-        public int ActivePlayerIndex => activePlayerIndex;
+        public string ActivePlayerId => activePlayerId;
 
 
         private bool chiActive;
         private bool pongActive;
         private bool kongActive;
         private bool huActive;
+        private GameManager gameManager;
+        private List<string> orderList = new List<string>();
 
 
-        void Start() 
+        void Start()
         {
+            gameManager = GameManager.Instance;
+
+            gameManager.TableManager = this;
+
+            InitPlayerControllerDict();
+
             tileWall.GetReady();
-            // todo: player will set player id, now is 0-3
-            for(int i = 0; i < 4; i++)
+
+            if (gameManager.Runner.IsServer)
             {
-                players[i].PlayerIndex = i;
-                players[i].TableManager = this;
+                // random pick order
+                RandomOrder();
+
+                // create player object and set info
+                SpawnAllPlayers();
+
+                // sync other player orderList
+                SyncPlayerOrder();
+
+                DealTiles();
+
+                SyncHandTiles();
             }
 
-            // PickSeatsAndDecideDealer();
-
-            DealTiles();
             OpenDoor();
         }
 
+        public void InitPlayerControllerDict()
+        {
+            foreach (var playerRef in gameManager.PlayerList.Keys)
+            {
+                if (roomManager.PlayerList.TryGetValue(playerRef, out PlayerController playerController))
+                    playerControllerDict.Add(playerController.PlayerID, playerController);
+            }
+        }
 
+        #region - host execute
 
+        public void RandomOrder()
+        {
+            foreach (string playerId in playerControllerDict.Keys)
+                orderList.Add(playerId);
+
+            for (int i = 0; i < orderList.Count; ++i)
+            {
+                int j = Random.Range(0, orderList.Count);
+                (orderList[i], orderList[j]) = (orderList[j], orderList[i]);
+            }
+        }
+
+        public void SpawnAllPlayers()
+        {
+            foreach (string playerId in playerControllerDict.Keys)
+            {
+                var playerObj = Instantiate(playerPrefab, contentTrans);
+                playerObj.SetInfo(playerId, this);
+                players.Add(playerId, playerObj);
+            }
+        }
+
+        public void SyncPlayerOrder()
+        {
+            for (int i = 0; i < orderList.Count; ++i)
+            {
+                if (playerControllerDict.TryGetValue(orderList[i], out PlayerController playerController))
+                    playerController.SetPlayerOrder_RPC(i);
+            }
+        }
+
+        public void DealTiles()
+        {
+            for (int round = 0; round < 4; ++round)
+            {
+                foreach (string playerId in orderList)
+                {
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        tileWall.DealTile(players[playerId]);
+                    }
+                }
+            }
+        }
+
+        public void SyncHandTiles()
+        {
+            foreach (string playerId in orderList)
+            {
+                if (playerControllerDict.TryGetValue(playerId, out PlayerController playerController))
+                {
+                    foreach (var tile in players[playerId].HandTiles)
+                    {
+                        string tileId = tile.GetComponent<Tile>().TileId;
+                        playerController.DealTiles_RPC(tileId);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region - client execute
+
+        public void SetPlayerOrder(string playerId, int order)
+        {
+            orderList[order] = playerId;
+        }
+
+        public void AssignTileToPlayer(string playerId, string tileId)
+        {
+            tileWall.DealTile(players[playerId], tileId);
+        }
+
+        #endregion
 
         void OpenDoor()
         {
-            tileWall.DealTile(players[activePlayerIndex]);
+            tileWall.DealTile(players[activePlayerId]);
             int cnt = 0;
-            while(cnt < 4)
+            while (cnt < 4)
             {
-                foreach(Player player in players)
+                foreach (Player player in players.Values)
                 {
                     if (player.IsDoneReplace())
                     {
@@ -71,51 +178,26 @@ namespace Game.Play {
                     }
                 }
             }
-            foreach(Player player in players)
+            foreach (Player player in players.Values)
             {
                 player.SortHandTiles();
             }
         }
 
-        void DealTiles()
-        {
-            for(int round = 0; round < 4; ++round)
-            {
-                for(int playerIndex = 0; playerIndex < 4; ++playerIndex)
-                {
-                    for(int i = 0; i < 4; ++i)
-                    {
-                        tileWall.DealTile(players[playerIndex]);
-                    }
-                }
-            }
-        }
-
-        void PickSeatsAndDecideDealer()
-        {
-
-            for(int i = 0; i < players.Count; ++i)
-            {
-                int j = Random.Range(0, players.Count);
-                (players[i], players[j]) = (players[j], players[i]);
-            }
-            activePlayerIndex = 0;
-        }
-
         private void Draw()
         {
-            tileWall.DealTile(players[activePlayerIndex]);
-            while(!players[activePlayerIndex].IsDoneReplace())
+            tileWall.DealTile(players[activePlayerId]);
+            while (!players[activePlayerId].IsDoneReplace())
             {
-                players[activePlayerIndex].ReplaceFlower();
+                players[activePlayerId].ReplaceFlower();
             }
 
-            if(activePlayerIndex == 0 && players[0].IsPlayerCanKong())
+            if (activePlayerId == orderList[0] && players[activePlayerId].IsPlayerCanKong())
             {
                 SetButton(kongBtn, true);
             }
 
-            if (activePlayerIndex == 0 && players[0].IsPlayerCanHu(true))
+            if (activePlayerId == orderList[0] && players[activePlayerId].IsPlayerCanHu(true))
             {
                 SetButton(winningBtn, true);
             }
@@ -123,29 +205,31 @@ namespace Game.Play {
 
         private void BuKong()
         {
-            tileWall.BuPai(players[activePlayerIndex]);
-            while(!players[activePlayerIndex].IsDoneReplace())
+            tileWall.BuPai(players[activePlayerId]);
+            while (!players[activePlayerId].IsDoneReplace())
             {
-                players[activePlayerIndex].ReplaceFlower();
+                players[activePlayerId].ReplaceFlower();
             }
         }
 
-        private void NextPlayer() 
+        public string NextPlayer()
         {
+            int activePlayerIndex = orderList.FindIndex(id => id == activePlayerId);
             activePlayerIndex = (activePlayerIndex + 1) % 4;
+            return orderList[activePlayerIndex];
         }
 
-        private void TurnToPlayer(int playerIndex)
+        private void TurnToPlayer(string playerId)
         {
-            activePlayerIndex = playerIndex;
+            activePlayerId = playerId;
         }
         //  only from single player
         private void AutoPlay()
         {
-            if(activePlayerIndex != 0)
+            if (activePlayerId != "")
             {
-                
-                players[activePlayerIndex].DefaultDiscard();
+
+                players[activePlayerId].DefaultDiscard();
             }
         }
 
@@ -174,51 +258,51 @@ namespace Game.Play {
         {
             huActive = true;
             int winningPlayerIndex = 0;
-            TurnToPlayer(winningPlayerIndex);
+            TurnToPlayer(orderList[winningPlayerIndex]);
             EndGame(winningPlayerIndex);
         }
 
         private void EndGame(int winningPlayerIndex)
         {
-            roundPoints.SetActive(true);
-            players[winningPlayerIndex].CallCalculator();
+            // roundPoints.SetActive(true);
+            // players[orderList[winningPlayerIndex]].CallCalculator();
 
-            points.text = "共 "+players[winningPlayerIndex].TaiCalculator.Tai+" 台";
+            // points.text = "共 " + players[orderList[winningPlayerIndex]].TaiCalculator.Tai + " 台";
 
-            int pointsChange = 300 + 100 * players[winningPlayerIndex].TaiCalculator.Tai; //還沒接上 gameManager 先用這個
-            // int pointsChange = gameManager.GameBasePoint + gameManager.GameTaiPoint * players[winningPlayerIndex].TaiCalculator.Tai;
+            // int pointsChange = 300 + 100 * players[orderList[winningPlayerIndex]].TaiCalculator.Tai; //還沒接上 gameManager 先用這個
+            // // int pointsChange = gameManager.GameBasePoint + gameManager.GameTaiPoint * players[winningPlayerIndex].TaiCalculator.Tai;
 
-            if (players[winningPlayerIndex].IsSelfDraw)
-            {
-                pointsChangeText[winningPlayerIndex].text = "+" + (pointsChange * 3).ToString();
-                pointsChangeText[(winningPlayerIndex + 1) % 4].text = "-" + pointsChange.ToString();
-                pointsChangeText[(winningPlayerIndex + 2) % 4].text = "-" + pointsChange.ToString();
-                pointsChangeText[(winningPlayerIndex + 3) % 4].text = "-" + pointsChange.ToString();
-            }
-            else
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    if (LastTile.GetComponent<Tile>().PlayerIndex == i)
-                    {
-                        pointsChangeText[i].text = "-" + pointsChange.ToString();
-                    }
-                    else
-                    {
-                        pointsChangeText[i].text = "+0";
-                    }
-                }
-                pointsChangeText[winningPlayerIndex].text = "+" + pointsChange.ToString();
-            }
+            // if (players[orderList[winningPlayerIndex]].IsSelfDraw)
+            // {
+            //     pointsChangeText[winningPlayerIndex].text = "+" + (pointsChange * 3).ToString();
+            //     pointsChangeText[(winningPlayerIndex + 1) % 4].text = "-" + pointsChange.ToString();
+            //     pointsChangeText[(winningPlayerIndex + 2) % 4].text = "-" + pointsChange.ToString();
+            //     pointsChangeText[(winningPlayerIndex + 3) % 4].text = "-" + pointsChange.ToString();
+            // }
+            // else
+            // {
+            //     for (int i = 0; i < 4; i++)
+            //     {
+            //         if (LastTile.GetComponent<Tile>().PlayerIndex == i)
+            //         {
+            //             pointsChangeText[i].text = "-" + pointsChange.ToString();
+            //         }
+            //         else
+            //         {
+            //             pointsChangeText[i].text = "+0";
+            //         }
+            //     }
+            //     pointsChangeText[winningPlayerIndex].text = "+" + pointsChange.ToString();
+            // }
 
-            List<string> scoringList = players[winningPlayerIndex].TaiCalculator.ScoringList;
+            // List<string> scoringList = players[orderList[winningPlayerIndex]].TaiCalculator.ScoringList;
 
-            InitScoringList();
+            // InitScoringList();
 
-            for (int i = 0; i < scoringList.Count; i++)
-            {
-                winningType[i].text = scoringList[i];
-            }
+            // for (int i = 0; i < scoringList.Count; i++)
+            // {
+            //     winningType[i].text = scoringList[i];
+            // }
         }
 
         private void InitScoringList()
@@ -232,58 +316,58 @@ namespace Game.Play {
         public IEnumerator BeforeNextPlayer()
         {
             // only control local player's button
-            if(players[0].IsPlayerCanChi())
-            {
-                SetButton(chiBtn, true);
-            }
-            if(players[0].IsPlayerCanPong())
-            {
-                SetButton(pongBtn, true);
-            }
-            if(players[0].IsPlayerCanKong())
-            {
-                SetButton(kongBtn, true);
-            }
-            if(players[0].IsPlayerCanHu(false))
-            {
-                SetButton(winningBtn, true);
-            }
+            // if (players[0].IsPlayerCanChi())
+            // {
+            //     SetButton(chiBtn, true);
+            // }
+            // if (players[0].IsPlayerCanPong())
+            // {
+            //     SetButton(pongBtn, true);
+            // }
+            // if (players[0].IsPlayerCanKong())
+            // {
+            //     SetButton(kongBtn, true);
+            // }
+            // if (players[0].IsPlayerCanHu(false))
+            // {
+            //     SetButton(winningBtn, true);
+            // }
             Debug.Log("開始停頓");
             // StartCoroutine(Countdown(3));
             yield return new WaitForSeconds(2f);
             Debug.Log("停頓結束");
-            SetButton(chiBtn, false);   
-            SetButton(pongBtn, false); 
+            SetButton(chiBtn, false);
+            SetButton(pongBtn, false);
             SetButton(kongBtn, false);
             SetButton(winningBtn, false);
             // todo: need to deal multiplayer move
-            if ( huActive )
+            if (huActive)
             {
 
             }
-            else if ( kongActive )
+            else if (kongActive)
             {
-                TurnToPlayer(0);
+                TurnToPlayer(orderList[0]);
                 kongActive = false;
                 BuKong();
             }
-            else if ( pongActive )
+            else if (pongActive)
             {
-                TurnToPlayer(0);
+                TurnToPlayer(orderList[0]);
                 pongActive = false;
             }
-            else if ( chiActive )
+            else if (chiActive)
             {
-                TurnToPlayer(0);
+                TurnToPlayer(orderList[0]);
                 chiActive = false;
             }
             else
             {
-                NextPlayer();
+                TurnToPlayer(NextPlayer());
                 Draw();
                 AutoPlay();
             }
-            
+
         }
 
     }
